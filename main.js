fc0e221afbb36902732f4451a16220b977f6a41a
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-03 14:20:50 +0100

    link

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 function addStudent() {
-    const studentName = document.getElementById("studentName").value;
-    const roomNumber = document.getElementById("roomNumber").value;
+    var studentName = document.getElementById("studentName").value;
+    var roomNumber = document.getElementById("roomNumber").value;
 
     if (studentName && roomNumber) {
-        const studentList = document.getElementById("studentList");
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(`Student Name: ${studentName}, Room Number: ${roomNumber}`));
+        var studentList = document.getElementById("studentList");
+        var li = document.createElement("li");
+
+        // Create a delete icon (you can use any icon library or custom icon here)
+        var deleteIcon = document.createElement("span");
+        deleteIcon.className = "delete-icon";
+        deleteIcon.innerHTML = "&#x1F5D1"; // Unicode for a delete icon (you can customize this)
+
+        // Create text node with student information
+        var studentInfo = document.createTextNode(`Student Name: ${studentName}, Room Number: ${roomNumber}`);
+
+        // Append delete icon and student information to the list item
+        li.appendChild(deleteIcon);
+        li.appendChild(studentInfo);
+
+        // Append the list item to the student list
         studentList.appendChild(li);
 
         // Clear input fields after adding student
         document.getElementById("studentName").value = "";
         document.getElementById("roomNumber").value = "";
+
+        // Attach click event to the delete icon to remove the corresponding list item
+        deleteIcon.addEventListener("click", function() {
+            studentList.removeChild(li);
+        });
     } else {
         alert("Please enter student name and room number.");
     }
